22f528d326712629fb2ce9c5da763e57949e2797
Mychael Castro <mychael.castro@kfc.com.ec>
2023-01-09 13:02:32 -0500

    cambios

--- src/Environment/config.js
+++ src/Environment/config.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const BASE_URL = "http://zone-drop-travel-back.test/api";
+const BASE_URL = "http://zone-drop-travel-back.test/api";
 // const BASE_URL = "https://dropxonetravel.herokuapp.com/api";
 //PRODUCCION
-const BASE_URL = "https://reserva.dropzonetravel.com.ec/api";
+// const BASE_URL = "https://reserva.dropzonetravel.com.ec/api";
 const API = axios.create({
   baseURL: BASE_URL,
 });
